dce227eafb3fda908e0747c91aab68b358c9e604
VictorG80 <164442787+VictorG80@users.noreply.github.com>
2024-03-21 20:50:09 -0500

    Add files via upload

---
+++ Ejercicio 1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.circulo;
+
+/**
+ *
+ * @author User
+ */
+public class Circulo {
+    
+    private double radio = 12.5;
+    private String color = "azul";
+
+    public Circulo() {}
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public double getArea() {
+        return Math.PI * radio * radio;
+    }
+}
